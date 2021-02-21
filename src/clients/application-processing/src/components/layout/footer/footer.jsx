40b985864b70e51daa80490600e32dc876c58ac4
Ali Soltani <soltani1384@gmail.com>
2021-02-21 13:12:36 +0330

    adding layout

---
+++ src/clients/application-processing/src/components/layout/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class footer extends Component {
+  state = {};
+  render() {
+    return <footer>footer</footer>;
+  }
+}
+
+export default footer;
---
+++ src/clients/application-processing/src/components/layout/main/main.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class main extends Component {
+  state = {};
+  render() {
+    return <span>Main</span>;
+  }
+}
+
+export default main;
